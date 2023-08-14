a288c2d709cad17f3a392757878ba48d05e3f9fb
Standa Vin <vinninjaq@gmail.com>
2023-08-14 11:32:26 +0200

    service: auth by Google Provider

--- my-app/src/app/components/Header/page.tsx
+++ my-app/src/app/components/Header/page.tsx
@@ -5,12 +5,15 @@ export default function Header() {
         <nav className="headerContainer">
             <div className="linksContainer">
                 <ul className="linksList">
+
                     <li className="listElement">
                         <Link href='/'>Home</Link>
                     </li>
+
                     <li className="listElement">
                         <Link href='/pages/aboutPage'>About</Link>
                     </li>
+                    
                 </ul>
             </div>
         </nav>
