953452fe6d1531365a0c55032011f80aaee12380
Standa Vin <vinninjaq@gmail.com>
2023-08-21 15:48:02 +0200

    feature: Editor page

---
+++ src/app/pages/editorPage/page.tsx
@@ -0,0 +1,7 @@
+export default function EditorPage() {
+    return (
+        <main className="mx-10">
+            <h1 className="px-2 py-2 mx-2 my-2 text-3xl flex items-center justify-center">Editor Page</h1>
+        </main>
+    )
+}
