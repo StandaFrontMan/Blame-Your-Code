c5c63a05af7cf68045bc1cf864a85776da4e4e1b
Standa Vin <vinninjaq@gmail.com>
2023-09-07 20:51:39 +0200

    service: Registration Page

---
+++ src/app/pages/registration/page.tsx
@@ -0,0 +1,8 @@
+export default function Regisration() {
+    return (
+        <main className="mx-10">
+            <h1 className="px-2 py-2 mx-2 my-2 text-3xl flex items-center justify-center">Registration Page</h1>
+
+        </main>
+    )
+}
