c3c3d12faf6725fec6d2d4daeefb7ec43ba60321
Stanislav Vin <vinninjaq@gmail.com>
2023-11-17 15:50:43 +0100

    refactor: divide Registration form on container and view, remove in src/app/modules/registration/ui/registration_form

---
+++ src/app/modules/registration/ui/registration_form/RegistrationForm.container.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import RegistrationFormView from "./RegistrationForm.view";
+
+export default function RegistrationFormContainer() {
+  const [formData, setFormData] = React.useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <RegistrationFormView
+      formData={formData}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
+}
---
+++ src/app/modules/registration/ui/registration_form/RegistrationForm.view.tsx
@@ -0,0 +1,67 @@
+type Props = {
+  handleChange: (e: any) => void;
+  handleSubmit: (e: any) => void;
+  formData: {
+    userName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
+};
+
+export default function RegistrationFormView({
+  formData,
+  handleSubmit,
+  handleChange,
+}: Props) {
+  return (
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={handleSubmit}
+    >
+      <div className="px-2 py-2 mx-2 my-2 flex flex-col justify-center items-center">
+        <input
+          className="px-2 py-2 mx-2 my-1 text-gray-800 bg-blue-500 hover:bg-blue-600 rounded-lg duration-150 outline-none placeholder-white"
+          placeholder="User Name..."
+          type="name"
+          name="name"
+          value={formData.userName}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="px-2 py-2 mx-2 my-1 text-gray-800 bg-blue-500 hover:bg-blue-600 rounded-lg duration-150 outline-none placeholder-white"
+          placeholder="Email..."
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="px-2 py-2 mx-2 my-1 text-gray-800 bg-blue-500 hover:bg-blue-600 rounded-lg duration-150 outline-none placeholder-white"
+          placeholder="Password..."
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="px-2 py-2 mx-2 my-1 text-gray-800 bg-blue-500 hover:bg-blue-600 rounded-lg duration-150 outline-none placeholder-white"
+          placeholder="Confirm Password..."
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <button className="py-2 px-2 mx-2 my-2 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg duration-150">
+          Register
+        </button>
+      </div>
+    </form>
+  );
+}
