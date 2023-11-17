d128e6a028b250163c65521309c3ad8c10f6fb50
Stanislav Vin <vinninjaq@gmail.com>
2023-11-17 16:14:09 +0100

    feature: DiContainer and AppDiContainer update

--- src/app/di/AppDiContainer.ts
+++ src/app/di/AppDiContainer.ts
@@ -2,6 +2,8 @@ import { UserDataRepositoryImpl } from "../core/user/data/UserDataRepositoryImpl
 import { UserDataRepository } from "../core/user/domain/UserDataRepository";
 import { AuthRepositoryImpl } from "../modules/Auth/data/user_credentials/AuthRepositoryImpl";
 import { AuthRepository } from "../modules/Auth/domain/user_credentials/AuthRepository";
+import { UserRegistrationRepositoryImpl } from "../modules/registration/data/UserRegistrationRepositoryImpl";
+import { UserRegistrationRepository } from "../modules/registration/domain/UserRegistrationRepository";
 import { DiContainer } from "./DiContainer";
 
 export const appDiContainer: DiContainer = new (class implements DiContainer {
@@ -11,5 +13,9 @@ export const appDiContainer: DiContainer = new (class implements DiContainer {
 
   getUserDataRepository(): UserDataRepository {
     return new UserDataRepositoryImpl();
+  }
+
+  getUserRegistrationRepository(): UserRegistrationRepository {
+    return new UserRegistrationRepositoryImpl();
   }
 })();
--- src/app/di/DiContainer.ts
+++ src/app/di/DiContainer.ts
@@ -1,7 +1,9 @@
 import { AuthRepository } from "../modules/Auth/domain/user_credentials/AuthRepository";
 import { UserDataRepository } from "../core/user/domain/UserDataRepository";
+import { UserRegistrationRepository } from "../modules/registration/domain/UserRegistrationRepository";
 
 export interface DiContainer {
   getAuthRepository(): AuthRepository;
   getUserDataRepository(): UserDataRepository;
+  getUserRegistrationRepository(): UserRegistrationRepository;
 }
