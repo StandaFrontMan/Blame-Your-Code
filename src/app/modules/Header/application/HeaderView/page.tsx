164d7a9ec46cc2c38879a5247a4b00dc80006b3b
Stanislav Vin <vinninjaq@gmail.com>
2023-11-20 20:21:32 +0100

    delete redundantn src/app/modules/Header

--- src/app/modules/Header/application/HeaderView/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import HeaderContainer from "../HeaderContainer/page";
-
-export default function HeaderView() {
-  return <HeaderContainer />;
-}
--- src/app/modules/Header/application/Isession.ts
+++
@@ -1,5 +0,0 @@
-import { Session } from "next-auth";
-
-export interface ISession {
-    session: Session | null
-}
