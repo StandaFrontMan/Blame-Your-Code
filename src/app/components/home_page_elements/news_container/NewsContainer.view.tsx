d07ffb01fbfc00e59cb533092c96551ddeb24d40
Stanislav Vin <vinninjaq@gmail.com>
2023-11-23 14:10:49 +0100

    fix: design

--- src/app/components/home_page_elements/news_container/NewsContainer.view.tsx
+++ src/app/components/home_page_elements/news_container/NewsContainer.view.tsx
@@ -7,22 +7,9 @@ type Props = {
 
 export default function NewsContainerView({ children, date }: Props) {
   return (
-    <div className="border-2">
+    <div className={styles.container}>
       <div className={styles.headerDate}>Project UpDates by {date}</div>
-      <div className={styles.linksArea}>
-        <a
-          href="https://github.com/StandaFrontMan/Blame-Your-Code"
-          className={styles.link}
-        >
-          GitHub
-        </a>
-        <a
-          href="https://github.com/Emelyanov535/Blame-Your-Code"
-          className={styles.link}
-        >
-          GitHub
-        </a>
-      </div>
+
       <div className={styles.updatesArea}>{children}</div>
     </div>
   );
