493803e379c3892f6195be94aea68d06e270ff35
Stanislav Vin <vinninjaq@gmail.com>
2023-12-03 20:35:47 +0100

    feature: editorPage styled

--- src/app/modules/create_post/ui/PostFormSubmition.view.tsx
+++ src/app/modules/create_post/ui/PostFormSubmition.view.tsx
@@ -1,7 +1,6 @@
 import { Editor } from "@monaco-editor/react";
 import { LanguagesArrayEntity } from "../data/LanguagesArrayEntity";
-import { Dispatch, SetStateAction } from "react";
-
+import styles from "./PostFormSubmition.module.scss";
 type Props = {
   formData: {
     postTitle: string;
@@ -30,48 +29,51 @@ export default function PostFormSubmitionView({
   usedLanguage,
 }: Props) {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Editor
-          height="90vh"
-          theme="vs-dark"
-          defaultLanguage={usedLanguage.language}
-          defaultValue={usedLanguage.value}
-          path={usedLanguage.name}
-          onMount={handleEditorDidMount}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={handleChange}
-          placeholder="title"
-          name="postTitle"
-          value={formData.postTitle}
-        />
-        <input
-          type="text"
-          onChange={handleChange}
-          placeholder="comment"
-          name="postComment"
-          value={formData.postComment}
-        />
-        <select
-          className="w-24 h-10 px-2 py-2 mx-2 my-1 text-white bg-gray-800 hover:bg-gray-900 rounded-lg duration-150 outline-none"
-          value={formData.postLanguage}
-          onChange={(e) =>
-            setFormData({ ...formData, postLanguage: e.target.value })
-          }
-        >
-          <option value="script.js">JS</option>
-          <option value="python.py">PY</option>
-          <option value="java.ja">Java</option>
-        </select>
-      </div>
-
-      <div>
-        <button>Submit</button>
-      </div>
-    </form>
+    <div className={styles.contanier}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.formContanier}>
+          <Editor
+            height="90vh"
+            theme="vs-dark"
+            defaultLanguage={usedLanguage.language}
+            defaultValue={usedLanguage.value}
+            path={usedLanguage.name}
+            onMount={handleEditorDidMount}
+          />
+        </div>
+        <div className={styles.inputsContanier}>
+          <select
+            // className="w-24 h-10 px-2 py-2 mx-2 my-1 text-white bg-gray-800 hover:bg-gray-900 rounded-lg duration-150 outline-none"
+            className={styles.select}
+            value={formData.postLanguage}
+            onChange={(e) =>
+              setFormData({ ...formData, postLanguage: e.target.value })
+            }
+          >
+            <option value="script.js">JS</option>
+            <option value="python.py">PY</option>
+            <option value="java.ja">Java</option>
+          </select>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="title"
+            name="postTitle"
+            value={formData.postTitle}
+            className={styles.titleInput}
+          />
+          <textarea
+            onChange={handleChange}
+            placeholder="comment"
+            name="postComment"
+            value={formData.postComment}
+            className={styles.textarea}
+          />
+          <div className={styles.buttonContanier}>
+            <button className={styles.button}>Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
