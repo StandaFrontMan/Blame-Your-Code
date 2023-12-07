80bbe28d893da5004f8fbe9bca45bf72709d5224
Stanislav Vin <vinninjaq@gmail.com>
2023-12-07 21:51:56 +0100

    fix: lazy loading on home page implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 "use client";
 
 import { Metadata } from "next";
-import React from "react";
+import { Suspense } from "react";
+import React, { lazy } from "react";
 import HomeHeaderContainer from "./components/headers/header_on_home_page/HomeHeader.container";
-import MediumContainerContainer from "./components/containers/medium_container/MediumContainer.container";
-import SmallContainerContainer from "./components/containers/small_container/SmallContainer.container";
+import LoaderView from "./components/loader/Loader.view";
 import LargeContainerContainer from "./components/containers/large_container/LargeContainer.container";
 
-import ShortDescriptionView from "./modules/home/ui/short_description/ShortDescription.view";
-import ExtraLargeContainerContainer from "./components/containers/extra_large_container/ExtraLargeContainer.container";
-import NewsContainerContainer from "./components/home_page_elements/news_container/NewsContainer.container";
-import FirstFrontNewsView from "./components/home_page_elements/news_container/news_mocks/FirstFrontNews.view";
-import FirstBackNewsView from "./components/home_page_elements/news_container/news_mocks/FirstBackNews.view";
-import HeaderView from "./components/headers/side_bar/Header.view";
+const LazyNewsContainerContainer = lazy(
+  () =>
+    import(
+      "./components/home_page_elements/news_container/NewsContainer.container"
+    )
+);
+const LazyFirstFrontNewsView = lazy(
+  () =>
+    import(
+      "./components/home_page_elements/news_container/news_mocks/FirstFrontNews.view"
+    )
+);
+const LazyFirstBackNewsView = lazy(
+  () =>
+    import(
+      "./components/home_page_elements/news_container/news_mocks/FirstBackNews.view"
+    )
+);
+const LazyShortDescriptionView = lazy(
+  () => import("./modules/home/ui/short_description/ShortDescription.view")
+);
 
 export const metadata: Metadata = {
   title: "Home",
@@ -22,18 +37,18 @@ export default function Home() {
   return (
     <main className="mx-10">
       <HomeHeaderContainer />
-
-      {/* <HeaderView /> */}
-
       <LargeContainerContainer>
-        <ShortDescriptionView />
+        <Suspense fallback={<LoaderView />}>
+          <LazyShortDescriptionView />
+        </Suspense>
       </LargeContainerContainer>
-
       <LargeContainerContainer>
-        <NewsContainerContainer date="22.11.23">
-          <FirstFrontNewsView />
-          <FirstBackNewsView />
-        </NewsContainerContainer>
+        <Suspense fallback={<LoaderView />}>
+          <LazyNewsContainerContainer date="22.11.23">
+            <LazyFirstFrontNewsView />
+            <LazyFirstBackNewsView />
+          </LazyNewsContainerContainer>
+        </Suspense>
       </LargeContainerContainer>
     </main>
   );
