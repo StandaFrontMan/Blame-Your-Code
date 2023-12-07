6582b39bb390153555d3a6a141c93d72a48d156f
Stanislav Vin <vinninjaq@gmail.com>
2023-12-07 22:04:58 +0100

    fix: registration page lazy loading implemented

--- src/app/pages/registration/page.tsx
+++ src/app/pages/registration/page.tsx
@@ -1,9 +1,16 @@
-import RegistrationFormContainer from "@/app/modules/sign_up/ui/sign_up_form/RegistrationForm.container";
+import LoaderContainer from "@/app/components/loader/Loader.contanier";
+import { lazy, Suspense } from "react";
 
+const LazyRegistrationFormContainer = lazy(
+  () =>
+    import("@/app/modules/sign_up/ui/sign_up_form/RegistrationForm.container")
+);
 export default async function Regisration() {
   return (
-    <main className="mx-10">
-      <RegistrationFormContainer />
-    </main>
+    <Suspense fallback={<LoaderContainer />}>
+      <main className="mx-10">
+        <LazyRegistrationFormContainer />
+      </main>
+    </Suspense>
   );
 }
